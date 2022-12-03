c26cdc7cbb08b204141e003b4cb2c79cc02174a7
Situ2000 <yuanyuansitu@gmail.com>
2022-12-03 01:56:05 +0000

    Create generateRandomString function

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,10 +25,15 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Pass along the urlDatabase to the file urls_index.
+// Pass along the urlDatabase to the urls_index template.
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// Create new route to render the urls_new template.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 // Create new route /urls/:id, the content will be shown when add the keyword id for searching.
@@ -36,7 +42,24 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 // Show the port number in the terminal.
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+// Generate a string of 6 random alphanumeric characters.
+function generateRandomString() {
+  let result = '';
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for (let i = 6; i > 0; i--) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+};
