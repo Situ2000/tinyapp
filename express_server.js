98b3c30823dbc89b0755b89d47967fa5b87c01fd
Situ2000 <yuanyuansitu@gmail.com>
2023-03-19 20:04:49 +0000

    Move database to database.js

--- express_server.js
+++ express_server.js
@@ -3,7 +3,8 @@ const cookieSession = require('cookie-session')
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080; // default port 8080
-const { generateRandomString, getUserByEmail, urlsForUser } = require('./helpers')
+const { generateRandomString, getUserByEmail, urlsForUser } = require('./helpers');
+const { urlDatabase, users } = require('./database');
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -12,31 +13,8 @@ app.use(cookieSession({
   keys: ["lighthouse"],
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+}));
 
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
-
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
 
 
 // Show "hello" message in the home website.
